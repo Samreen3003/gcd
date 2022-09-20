f49cbb1928ee1a1fb53d53f60a40084adf2d0521
Samreen3003 <113880222+Samreen3003@users.noreply.github.com>
2022-09-20 13:57:54 +0530

    Add files via upload

---
+++ descending.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+char nameA11[5][20],nameOne[20];
+int i,j;
+cout<<"enter=";
+for(i=0;i<5;i++)
+    cin>>nameAll[i];
+for(i=1;i<5;i++)
+{
+     for(j=1;j<5;j++)
+     {
+if(strcmp(nameAll[j-1],nameAll[j]>0)
+{
+     strcpy(nameOne,nameAll[j-1]);
+     strcpy(namAll[j-1],nameAll[j]);  
+     strcpy(nameAll[j],nameOne);
+}
+}
+}
+{
+cout<<"\nStrings(Names)in Alphabetical order=\n";
+for(i=0;i<5;i++)
+     cout<<nameAll[i]<<endl;
+cout<<endl;
+char str[5][20]={"apple","banana","jack","carrot","radish]
+char t[20];
+int i,j;
+for(i=1;i<5;i++)
+      cout<<nameAll[i]<<endl;
+cout<<endl;
+char str[5][20]={"appole","banana","jack","carrot","raddish"};
+char t[20];
+int i,j;
+for(i=1;i<5;i++)
+{
+  for(j=1;j<5;j++)
+  {
+    if(strcmp(strt[j-1],str[j]>0))
+    {
+      strcpy(t,str[j-1]);
+      strcpy(str[j-1],str[j]);
+      strcpy(str[j],t);
+  }
+}
+}
+cout<<"sorted in descending order = ";
+for(i=5;i>=0;i--)
+{
+  cout<<" ";
+  cout<<str[i]<<"\n";
+}
+return 0;
+}
